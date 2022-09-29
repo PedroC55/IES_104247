0da5001de729fbf2c17b53e3e8b179d79b48825a
Pedro <plbc@ua.pt>
2022-09-29 13:44:34 +0100

    location2

--- lab1/lab1_2/my-weather-radar/src/main/java/com/myweather/WeatherStarter.java
+++ lab1/lab1_2/my-weather-radar/src/main/java/com/myweather/WeatherStarter.java
@@ -6,6 +6,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import com.myweather.IpmaCityForecast; //may need to adapt package name
 import com.myweather.IpmaService;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 /**
  * demonstrates the use of the IPMA API for weather forecast
@@ -14,7 +17,7 @@ public class WeatherStarter {
 
     //todo: should generalize for a city passed as argument
     //private static final int CITY_ID_AVEIRO = 1010500;
-
+    private static Logger logger = LogManager.getLogger(WeatherStarter.class);
     public static void  main(String[] args ) {
         int codigo = Integer.parseInt(args[0]);
         // get a retrofit instance, loaded with the GSon lib to convert JSON into objects
@@ -35,11 +38,11 @@ public class WeatherStarter {
             if (forecast != null) {
                 var firstDay = forecast.getData().listIterator().next();
 
-                System.out.printf( "\nCountry: %s \nDay: %s \nMax Temperature: %4.1f \nProbability of precipitation: %4.1f \n",
+                logger.info(String.format("\nCountry: %s \nDay: %s \nMax Temperature: %4.1f \nProbability of precipitation: %4.1f \n",
                         forecast.getCountry(),
                         firstDay.getForecastDate(),
                         Double.parseDouble(firstDay.getTMax()),        
-                        Double.parseDouble(firstDay.getPrecipitaProb()));
+                        Double.parseDouble(firstDay.getPrecipitaProb())));
                         
             } else {
                 System.out.println( "No results for this request!");
